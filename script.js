550215b9895da2ee6b0d377337a968fa61e9bdd7
harry01236 <142505910+harry01236@users.noreply.github.com>
2023-08-18 10:11:29 +0545

    Update script.js

--- script.js
+++ script.js
@@ -75,6 +75,50 @@ clickableImage.addEventListener('click', function() {
   window.open('https://www.google.com/maps/place/%E0%A4%A7%E0%A4%A8%E0%A5%81%E0%A4%B7%E0%A4%BE+%E0%A4%9C%E0%A4%BF%E0%A4%B2%E0%A5%8D%E0%A4%B2%E0%A4%BE/@26.8501393,85.4301205,10z/data=!3m1!4b1!4m6!3m5!1s0x39ec10de9587fa25:0x1372d7ca3b524b51!8m2!3d26.8350474!4d86.0121573!16zL20vMDhja3k2?entry=ttu', '_blank'); // Replace with your desired URL
 });
 
+//Typewritter js code
+const sentenceElement = document.getElementById('sentence');
+const sentences = [
+  "I'm Siddhant.",
+  "Turning Ideas into Reality.",
+  "Have a good visit to my website!"
+];
+
+let currentSentenceIndex = 0;
+let charIndex = 0;
+let isBlank = false;
+
+function type() {
+  if (currentSentenceIndex >= sentences.length) {
+    currentSentenceIndex = 0; // Reset to the first sentence
+  }
+  
+  const currentSentence = sentences[currentSentenceIndex];
+  if (charIndex < currentSentence.length) {
+    sentenceElement.textContent += currentSentence.charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 100); // Adjust the typing speed here (in milliseconds)
+  } else {
+    if (!isBlank) {
+      isBlank = true;
+      setTimeout(erase, 1500); // Wait before erasing
+    }
+  }
+}
+
+function erase() {
+  if (sentenceElement.textContent.length > 0) {
+    sentenceElement.textContent = sentenceElement.textContent.slice(0, -1);
+    setTimeout(erase, 50); // Adjust the erasing speed here (in milliseconds)
+  } else {
+    isBlank = false;
+    charIndex = 0;
+    currentSentenceIndex++;
+    setTimeout(type, 500); // Wait before typing next sentence
+  }
+}
+
+type();
+
 
 
 
